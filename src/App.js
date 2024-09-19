a018180dd94dec6fb4fa820e5fa9fc71273f00a2
Emang-M <emangmachola@gmail.com>
2024-09-19 16:05:13 +0200

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <button> Click </button>
       <h1> React Weather App </h1>
+    </div>
     </div>
   );
 }
