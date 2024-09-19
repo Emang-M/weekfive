e222077999c75624b92eec0d44c30ec5c29a1c1d
Emang-M <emangmachola@gmail.com>
2024-09-19 16:18:13 +0200

    Updated html structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <button> Click </button>
-      <h1> React Weather App </h1>
-    </div>
+        <Weather defaultCity="Maun" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Emang-M"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Emang Machola
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/weekfive"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://updatewhendeployed.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
