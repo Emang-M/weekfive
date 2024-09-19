5afd249e222e51a4c71b2d88dc0206b1ebd64dc0
Emang-M <emangmachola@gmail.com>
2024-09-19 22:13:35 +0200

    updated Weather.js
    Added axios
    Used and Set States
    Created new functions
    Added event listeners for functions to work or respond
    Created new functions for readability  and for code to work and included them

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css"
 
-export default function Weather(){
-    return (
+export default function Weather(props){
+    let [weatherData, setWeatherData] = useState({ ready: false });
+    let [loaded, setLoaded] = useState(false);
+    let [city, setCity] = useState(props.city);
+
+    function handleResponse(response) {
+        console.log(response.data);
+        setLoaded(true);
+      setWeatherData({
+        coordinates: response.data.coord,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: new Date(response.data.dt * 1000),
+        description: response.data.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+      });
+    }
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      let apiKey = "dff5c692192605ee5ed7f95b423ae857";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+      axios.get(apiUrl).then(handleResponse);
+    }
+  
+    function handleCityChange(event) {
+      setCity(event.target.value);
+    }
+  
+
+
+   if(loaded) return (
         <div className="Weather">
-            <form> 
+            <form onSubmit={handleSubmit}> 
                 <div className="row">
                     <div className="col-9"> 
                         <input 
                         type="Search"
                         placeholder="Enter a city.."
                         className="form-control"
+                        autoFocus="on"
+                        onChange={handleCityChange}
                         />
                         </div>
                         <div className="col-3">
@@ -22,25 +59,7 @@ className="btn btn-primary w-100"
  </div>
                     </div>
             </form>
-            <h1>Maun</h1>
-            <ul>
-            <li> Thursday 19:42</li>
-            <li>Clear with periodic clouds</li>
-            </ul>
-            <div className="row mt-3">
-                <div className="clearfix">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="clear" className="float-left" />
-                <span className="temperature"> 30 </span>
-                <span className="unit"> °C </span>
+            < WeatherInfo data={weatherData} />
             </div>
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li> Precipitation: 1% </li>
-                    <li>Humidity: 20% </li>
-                    <li>Wind: 3 km/h </li>
-                    </ul>
-            </div>
-        </div>
-    );
+            );
 }
