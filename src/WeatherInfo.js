4a93031033cc82a60ddaeef8af13b62c1e4f8192
Emang-M <emangmachola@gmail.com>
2024-09-22 01:17:41 +0200

    fixed the bug

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,8 +19,7 @@ export default function WeatherInfo(props) {
             <div className="d-flex">
               <div>
               </div>
-              autoFocus="on"
-              <WeatherIcon code={props.data.icon} size={52} />
+                            <WeatherIcon code={props.data.icon} size={52} />
               <div>
                 <WeatherTemperature celsius={props.data.temperature} />
               </div>
