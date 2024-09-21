3bc527e49978b18e9fb0d4d55f8209074c06cf2c
Emang-M <emangmachola@gmail.com>
2024-09-22 01:20:21 +0200

    changed code because fahrenheit conversion isnt working

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
-import React,{useState} from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-let [unit,setUnit]=useState("celcius");
-
-function showFahrenheit(event){
-    event.preventDefault();
-setUnit("fahrenheit");
-}
-
-function showCelcius(event){
-    event.preventDefault();
-    setUnit("celcius");
-}
-
-function fahrenheit(props) {
-    return (props.celsius * 9) / 5 + 32;
-}
-
-if (unit=== "celcius"){
   return (
     <div className="WeatherTemperature">
-<span className="temperature">{Math.round(props.celsius)}</span>
-<span className="unit">°C |
-  <a href="/" onClick={showFahrenheit} rel="norefferer"
-  >°F 
-  </a> 
-  </span>
-<span className="unit">°C</span>
-</div>
-);
-} else {
-return(
-<div className="WeatherTemperature">
-<span className="temperature">{Math.round(fahrenheit)}</span>
-<span className="unit">°C 
-  <a href="/" onClick={showCelcius} rel="norefferer">
-  |°F
-  </a>
-  </span>
-</div>
-);
-}
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
