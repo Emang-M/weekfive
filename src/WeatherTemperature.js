845cbedad56575751c801f3c35483d55fd38a10d
Emang-M <emangmachola@gmail.com>
2024-09-21 23:05:16 +0200

    Created new component
    For temperature conversion, to show the temperature in celcius or fahrenheit.

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,41 @@
+import React,{useState} from "react";
+
+export default function WeatherTemperature(props) {
+let [unit,setUnit]=useState("celcius");
+
+function showFahrenheit(event){
+    event.preventDefault();
+setUnit("fahrenheit");
+}
+
+function showCelcius(event){
+    event.preventDefault();
+    setUnit("celcius");
+}
+
+function fahrenheit(){
+    return (props.celcius * 9) / 5 + 32;
+}
+
+if (unit===celcius){
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C| ("")
+        <a href="/" onClick={showFahrenheit} rel="norefferer">°F 
+        </a>
+        </span>
+    </div>
+  );
+} else {
+    return(
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(fahrenheit)}</span>
+      <span className="unit">°C| ("")
+        <a href="/" onClick={showCelcius} rel="norefferer">°F
+        </a>
+        </span>
+    </div>
+    );
+}
+}
