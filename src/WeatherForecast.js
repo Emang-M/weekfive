1246ef14e5c3944f208088a15c4d7405729f834d
Emang-M <emangmachola@gmail.com>
2024-09-27 22:20:50 +0200

    Fixing a bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,13 +16,12 @@ export default function WeatherForecast (props){
     
       if (loaded){
         return (
-         <div>
-            <div className="WeatherForecast-day"> {props.data.dt} </div>
-            <WeatherIcon code={props.data.weather[0].icon} size={36} />
-              <div className="WeatherForecast-temperatures">
-                <span className="WeatherForecast-temperature-max">{forecast[0].temp.max}°</span>
-                <span className="WeatherForecast-temperature-min">{forecast[0].temp.min}°</span>
+            <div className="WeatherForecast">
+            <div className="row">
+              <div className="col">
+                <WeatherForecastDay data={forecast[0]} />
               </div>
+            </div>
           </div>
       );
     } else {
